cd591aa06a7471d75c6c8f0e13535165404594ac
serzh <serzhan181@outlook.com>
2023-05-13 18:47:16 +0600

    feat: search for tracks

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -1,24 +1,9 @@
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Card } from "@/components/ui/card";
-import { Skeleton } from "@/components/ui/skeleton";
+import { LoadingSkeleton } from "./_components/skeleton";
 
 export default function SearchLoading() {
   return (
-    <div className="flex flex-col gap-3">
-      {Array(8)
-        .fill(0)
-        .map((_, i) => (
-          <Card key={i} className="flex overflow-hidden">
-            <div className="w-60 bg-background/50">
-              <AspectRatio ratio={1}>
-                <Skeleton className="absolute inset-0" />
-              </AspectRatio>
-            </div>
-            <div className="flex flex-col w-full gap-3 p-3">
-              <Skeleton className="w-[350px]" />
-            </div>
-          </Card>
-        ))}
+    <div className="grid w-full gap-5 bg-slate-600 sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-4">
+      <LoadingSkeleton />
     </div>
   );
 }
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,3 +1,101 @@
+"use client";
+
+import { Input } from "@/components/form/input";
+import { SongCard } from "@/components/song-card";
+import { Button } from "@/components/ui/button";
+import { useAppSelector } from "@/hooks/use-redux";
+import {
+  seekToSong,
+  setIsPlaying,
+  setPlaylist,
+} from "@/store/features/player-slice";
+import { useSearchTracksQuery } from "@/store/services/soundcloud-api";
+import { Search } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { LoadingSkeleton } from "./_components/skeleton";
+
 export default function SearchPage() {
-  return <div>search page</div>;
+  const searchParams = useSearchParams();
+
+  const query = searchParams?.get("q");
+
+  const { data, isLoading } = useSearchTracksQuery(query || "", {
+    skip: !query,
+  });
+
+  const dispatch = useDispatch();
+  const { curSong, curPlaylist } = useAppSelector((state) => state.player);
+
+  return (
+    <div className="w-full h-full">
+      <SearchInput />
+      <div className="grid gap-5 mt-3 sm:grid-cols-2 xs:grid-cols-1 md:grid-cols-4">
+        {isLoading && <LoadingSkeleton />}
+        {data && !isLoading && (
+          <>
+            {data.res.collection.map((t, idx) => (
+              <SongCard
+                key={t.id}
+                title={t.title}
+                authorName={t.user.username}
+                thumbnailUrl={
+                  t?.artwork_url?.replace("large", "t500x500") ||
+                  "/assets/images/agents/reyna.webp"
+                }
+                id={t.id}
+                curSongId={curSong?.id || -1}
+                onPlaySong={(songId) => {
+                  if (curPlaylist?.id !== data.res.collection[0].id) {
+                    dispatch(
+                      setPlaylist({
+                        id: data.res.collection[0].id,
+                        tracks: data.res.collection,
+                        artwork_url: data.res.collection[0].artwork_url,
+                        username: data.res.collection[0].user.username,
+                        idxInPlaylist: idx,
+                      })
+                    );
+                  } else {
+                    const idxInPlaylist = curPlaylist.tracks.findIndex(
+                      (t) => t.id === songId
+                    );
+                    dispatch(seekToSong(idxInPlaylist));
+                  }
+                }}
+                isPlaying={false}
+                onPause={() => dispatch(setIsPlaying(false))}
+                onPlay={() => dispatch(setIsPlaying(true))}
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
+
+interface SearchInputProps {}
+
+const SearchInput = (props: SearchInputProps) => {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/search?q=${encodeURIComponent(query)}`);
+  };
+  return (
+    <form className="flex gap-2" onSubmit={(e) => onSubmit(e)}>
+      <Input
+        placeholder="Search"
+        onChange={(e) => setQuery(e.target.value)}
+        value={query}
+      />
+      <Button type="submit">
+        <Search />
+      </Button>
+    </form>
+  );
+};
