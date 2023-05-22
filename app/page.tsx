051edeeab3aba51dd3e3cab9c4759882b6969774
serzh <serzhan181@outlook.com>
2023-05-22 16:44:32 +0600

    feat: phonk instead popular

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import { TracksSection } from "@/components/sections/tracks-section";
 import { sc } from "@/config/soundcloud";
 
 export default async function Home() {
-  const tracks = await sc.tracks.searchV2({ q: "" });
+  const tracks = await sc.tracks.searchV2({ q: "phonk" });
   const workoutTracks = await sc.tracks.searchV2({ q: "workout" });
   const chillTracks = await sc.tracks.searchV2({ q: "lofi|relax|jazz" });
   const playlists = await sc.playlists.searchV2({ q: "jazz", limit: 1 });
 
   return (
     <div className="flex flex-col gap-5">
-      <TracksSection tracks={tracks.collection} title="Popular." />
+      <TracksSection tracks={tracks.collection} title="Phonk." />
       <PlaylistSection playlist={playlists.collection[0]} title="Jazz." />
       <TracksSection
         tracks={chillTracks.collection}
